04058d6e187e3576086aa00fb0f2439666000dfd
davidmartin1991 <david.martin.jall@gmail.com>
2018-11-16 16:43:48 +0100

    test

--- app/src/test/java/fr/android/androidexercises/LoginPresenterTest.kt
+++ app/src/test/java/fr/android/androidexercises/LoginPresenterTest.kt
@@ -1,5 +1,38 @@
 package fr.android.androidexercises
 
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verifyAll
+import org.junit.Before
+import org.junit.Test
+
 class LoginPresenterTest {
-    // TODO test login presenter with JUnit and MockK
+
+    @RelaxedMockK
+    private lateinit var activity: LoginActivity
+    private lateinit var presenter: LoginPresenter
+
+    @Before
+    fun setup(){
+        MockKAnnotations.init(this)
+        presenter = LoginPresenter(activity)
+    }
+    @Test
+    fun testCheckCreditentialsCorrect(){
+        presenter.checkCredentials("my pass")
+        verifyAll {
+            activity.logged()
+            activity.message(R.string.text_logged)
+        }
+    }
+
+    @Test
+    fun testCheckCreditentialsIncorrect(){
+        presenter.checkCredentials("my")
+        verifyAll {
+            activity.notLogged()
+            activity.message(R.string.notLogged)
+        }
+    }
 }
